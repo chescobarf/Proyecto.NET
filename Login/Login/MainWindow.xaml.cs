eebeca84aed71b948b69984ed40238a237f48e9b
Ch.Escobar <christian17zxe@gmail.com>
2019-08-23 13:49:05 -0400

    LoginMetro

--- Login/Login/MainWindow.xaml.cs
+++ Login/Login/MainWindow.xaml.cs
@@ -12,13 +12,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
 
 namespace Login
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
