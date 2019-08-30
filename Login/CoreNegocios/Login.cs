e2353b2b25f8ed72a4150b556aa3578ef524e935
Christian <christian17zxe@gmail.com>
2019-08-30 17:03:25 -0400

    Core Negocios

---
+++ Login/CoreNegocios/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreNegocios
+{
+   public class Login
+    {
+        public string PASSWORD { get; set; }
+        public int ESTADO { get; set; }
+        public decimal PERSONA_RUT { get; set; }
+
+        public Login()
+        {
+
+        }
+
+
+
+    }
+}
